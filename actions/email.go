2baf743567c00b2d1f090a870f1cd4eecec5c96b
Francesco Sacchi <batt@develer.com>
2019-04-02 11:00:19 +0200

    Merge recvmail feature.

---
+++ actions/email.go
@@ -0,0 +1,109 @@
+package actions
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/develersrl/lunches/pkg/brain"
+	"github.com/develersrl/lunches/pkg/tuttobene"
+	"github.com/gobuffalo/buffalo"
+	"github.com/mailgun/mailgun-go/v3"
+)
+
+// EmailHandler default implementation.
+func EmailHandler(c buffalo.Context) error {
+	log.Println("Email received!")
+	domain := os.Getenv("MAILGUN_DOMAIN")
+	if domain == "" {
+		log.Println("MAILGUN_DOMAIN not set")
+		return nil
+	}
+
+	apiKey := os.Getenv("MAILGUN_API_KEY")
+	if apiKey == "" {
+		log.Println("MAILGUN_API_KEY not set")
+		return nil
+	}
+
+	mg := mailgun.NewMailgun(domain, apiKey)
+
+	verified, err := mg.VerifyWebhookSignature(mailgun.Signature{
+		TimeStamp: c.Param("timestamp"),
+		Token:     c.Param("token"),
+		Signature: c.Param("signature"),
+	})
+
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	if !verified {
+		log.Printf("Mailgun signature verification error")
+		return nil
+	}
+
+	if !strings.HasPrefix(c.Param("Content-Type"), "multipart/mixed") {
+		log.Printf("Wrong POST Content-Type: '%s'", c.Param("Content-Type"))
+		return nil
+	}
+	if c.Param("attachment-count") == "" {
+		log.Println("No attachment found")
+		return nil
+	}
+
+	n, err := strconv.Atoi(c.Param("attachment-count"))
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	for i := 0; i < n; i++ {
+		f, h, err := c.Request().FormFile(fmt.Sprintf("attachment-%d", i+1))
+		if err != nil {
+			log.Println(err)
+			return nil
+		}
+		name := strings.ToLower(h.Filename)
+		if strings.Contains(name, "menu") && strings.Contains(name, ".xlsx") {
+			if h.Size > 500000 {
+				log.Println("Attachemnt too large!")
+				return nil
+			}
+			buf := make([]byte, h.Size)
+
+			_, err := f.Read(buf)
+			if err != nil {
+				log.Println(err)
+				return nil
+			}
+
+			m, err := tuttobene.ParseMenuBytes(buf)
+
+			if err != nil {
+				log.Println("Menu parse error: ", err)
+				return nil
+			}
+			redisURL := os.Getenv("REDIS_URL")
+			if redisURL == "" {
+				log.Println("No redis URL found!")
+				return nil
+			}
+
+			b := brain.New(redisURL)
+			defer b.Close()
+
+			b.Set("menu", *m)
+
+			log.Println("Tuttobene menu parsed correctly")
+			return nil
+		}
+
+		log.Println("Unrecognized attachment:", h.Filename)
+	}
+
+	log.Println("No menu parsed from email")
+	return nil
+}
